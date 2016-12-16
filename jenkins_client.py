771e7284288a9cc2ec8f3a91adf0b3a331f829c7
nick.d.rohn <nick.d.rohn@gmail.com>
2016-12-16 15:46:29 -0700

    replaced file

--- jenkins_client.py
+++ jenkins_client.py
@@ -1,6 +1,7 @@
 import os
 import json
 import jenkins
+import socket
 from six.moves.urllib.request import Request
 
 
@@ -10,40 +11,46 @@ ENV_JENKINS_POLL_CLIENT_USERNAME = 'jenkins_poll_client:username'
 ENV_JENKINS_POLL_CLIENT_PASSWORD = 'jenkins_poll_client:password'
 ENV_JENKINS_POLL_CLIENT_URL = 'jenkins_poll_client:url'
 ENV_JENKINS_POLL_CLIENT_JOB = 'jenkins_poll_client:job'
-ENV_JENKINS_POLL_CLIENT_scm_name = 'jenkins_poll_client:scm_name'
+
 PYTHON_JENKINS_SCM_POLL = '%(folder_url)sjob/%(short_name)s/polling'
 
 
 class JenkinsServer(jenkins.Jenkins):
-    """ we extend Jenkins to support SCM polling
+    """ we extend jenkins.Jenkins to support SCM polling
     """
-    def build_poll_scm_name(self, scm_name, job):
-        folder_url, short_name = self._get_job_folder(job)
+    def __init__(self, job, url, username=None, password=None,
+                 timeout=socket.getdefaulttimeout(),
+                 ):
+        self.job = job
+        super(JenkinsServer, self).__init__(url, username, password, timeout)
+
+    def build_poll_scm_name(self):
+        folder_url, short_name = self._get_job_folder(self.job)
         locals_ = {'folder_url': folder_url, 'short_name': short_name}
         poll_scm_name = self._build_url(PYTHON_JENKINS_SCM_POLL, locals_)
         return poll_scm_name
 
-    def poll_scm(self, scm_name, job):
-        request = Request(self.build_poll_scm_name(scm_name, job), b"")
+    def poll_scm(self):
+        request = Request(self.build_poll_scm_name(), b"")
         self.jenkins_open(request, True)
 
 
 class JenkinsConnection(object):
     """ stores connection information required
-    to use the Jenkins REST API
+    to use the Jenkins REST API. we essentially
+    use this to help decouple ourselves from 
+    jenkins.Jenkins
     """
     def __init__(self, config):
         if config is None:
             raise ValueError("config cannot be None")
         self.server = make_jenkins_server(config)
-        self.job = config.job
-        self.scm_name = config.scm_name
 
 
 class JenkinsConfig(object):
     """ stores Jenkins configuration info
     """
-    def __init__(self, username, password, url, job, scm_name):
+    def __init__(self, username, password, url, job):
         if username is None:
             raise ValueError("username cannot be None")
         if password is None:
@@ -52,13 +59,10 @@ class JenkinsConfig(object):
             raise ValueError("url cannot be None")
         if job is None:
             raise ValueError("job cannot be None")
-        if scm_name is None:
-            raise ValueError("scm_name cannot be None")
         self.url = url
         self.username = username
         self.password = password
         self.job = job
-        self.scm_name = scm_name
 
 
 def load_json_from_file(filepath):
@@ -76,7 +80,6 @@ def make_jenkins_config_json(filepath):
     config = load_json_from_file(filepath)
     url = config['url']
     job = config['job']
-    scm_name = config['scm_name']
     auth_header = config['auth']
     username = auth_header['username']
     password = auth_header['password']
@@ -84,8 +87,7 @@ def make_jenkins_config_json(filepath):
         username=username,
         password=password,
         url=url,
-        job=job,
-        scm_name=scm_name)
+        job=job)
 
 
 def get_env_variable(key):
@@ -95,15 +97,13 @@ def get_env_variable(key):
 def make_jenkins_config_env():
     url = get_env_variable(ENV_JENKINS_POLL_CLIENT_URL)
     job = get_env_variable(ENV_JENKINS_POLL_CLIENT_JOB)
-    scm_name = get_env_variable(ENV_JENKINS_POLL_CLIENT_scm_name)
     username = get_env_variable(ENV_JENKINS_POLL_CLIENT_USERNAME)
     password = get_env_variable(ENV_JENKINS_POLL_CLIENT_PASSWORD)
     return JenkinsConfig(
         username=username,
         password=password,
         url=url,
-        job=job,
-        scm_name=scm_name)
+        job=job)
 
 
 # builds a "Jenkins" objects used to connect
@@ -111,10 +111,12 @@ def make_jenkins_config_env():
 
 def make_jenkins_server(config):
     uri = config.url
+    job = config.job
     username = config.username
     password = config.password
 
     server = JenkinsServer(
+        job,
         uri,
         username=username,
         password=password
@@ -122,21 +124,8 @@ def make_jenkins_server(config):
     return server
 
 
-#  connects to a Jenkins REST API and requests
-
-def jenkins_whoami(connection):
-    server = connection.server
-    return server.get_whoami()
-
-
-def jenkins_build_job(connection):
-    server = connection.server
-    job = connection.job
-    return server.build_job(job)
-
+#  connects to a Jenkins REST API and polls SCM
 
 def jenkins_poll_scm(connection):
     server = connection.server
-    job = connection.job
-    scm_name = connection.scm_name
-    server.poll_scm(scm_name, job)
+    server.poll_scm()
